bae96cce9f1a4fbff847eede7959834fa3986d21
Harigovind SR <harigovindsr@outlook.com>
2024-07-05 19:57:48 +0530

    Continue work on project first-react-app

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',        // Proxy is used so instead of changing backend target
+        changeOrigin: true,                     // URL everywhere it only needs to be changed here
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
