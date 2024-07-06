f92982de44e0c005f3a1fa5788d26c5476713ee7
Harigovind SR <harigovindsr@outlook.com>
2024-07-06 23:36:06 +0530

    Project first-react-app day two

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,17 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import JobPage from "./pages/JobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path="/jobs" element={<JobsPage />} />
-    <Route path="/jobs/:id" element={<JobPage />} />
-                    {/* ↑ ':' in :id represents that it is dynamic */}
+    <Route path="/add-job" element={<AddJobPage />} />
+    <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+                    {/* ↑ ':' in :id represents that it is dynamic. It is a parameter that can be accessed using useParams() hook and by the params object passed on to the loader */}
     <Route path="*" element={<NotFoundPage />} />
   </Route>
   )
