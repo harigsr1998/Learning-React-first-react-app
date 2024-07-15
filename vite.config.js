4ea8eb6a6eb2d69a58c690ae7024811e1a3a455f
Harigovind SR <harigovindsr@outlook.com>
2024-07-15 20:01:07 +0530

    Continue deploy project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Learning-React-first-react-app/",
   plugins: [react()],
   server: {
     proxy: {
